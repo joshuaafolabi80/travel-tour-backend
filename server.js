4729b815a9928d586a4da0cb94dc2a2ca53e2ef4
Gbenga Joshua Afolabi <joshuaafolabi80@gmail.com>
2025-12-29 08:17:51 -0800

    c

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const fs = require('fs');
 const mammoth = require('mammoth');
 const multer = require('multer');
-const appReviewRoutes = require('./appReviewRoutes');
+const appReviewRoutes = require('./routes/appReviewRoutes');
 require('dotenv').config();
 
 const cloudinary = require('cloudinary').v2;
